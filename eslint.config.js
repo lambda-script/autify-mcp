f01f92845b97e775b01c36f5875bba70c8680d0d
maturu <maturu@lambdascript.dev>
2026-06-08 15:41:14 +0900

    chore: scaffold project tooling

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "no-console": "error",
+    },
+  },
+  { ignores: ["dist/", "node_modules/", "tests/", "src/generated/", "*.config.*"] },
+);
